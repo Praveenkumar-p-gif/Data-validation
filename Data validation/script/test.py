55164230038e7cfd0dea1b508fa06a84082b64ad
Praveenkumar-p-gif <praveen29083@gmail.com>
2025-03-03 08:56:05 +0530

    Add files via upload

---
+++ Data validation/script/test.py
@@ -0,0 +1,19 @@
+from Utilities import *
+
+res = nameValidation("Praveen")
+print(res)
+
+res = fileTypeValidation("abc.txt")
+print(res)
+
+res = fileTypeValidation("xyz.txt")
+print(res)
+
+res = filesizeValidation(r'plan.png')
+print(res)
+
+res = filesizeValidation(r'abc.txt')
+print(res)
+
+
+
